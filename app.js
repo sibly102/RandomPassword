4d3abe45e558b8eff91da386ffbc8a52d12498a5
sibly102 <86411230+sibly102@users.noreply.github.com>
2024-12-21 12:41:32 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("Passport");
+const length = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "123456789";
+const symbol = "!@$%^&*(()_+*-;'";
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function passwordCopy(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
